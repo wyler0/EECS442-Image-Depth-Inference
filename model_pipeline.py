9aaab9a1fe3b3958b8388df8d7a5ed002b09c0b0
Wyler Zahm <w3h3z3@gmail.com>
2021-04-18 17:59:49 -0400

    beginning integration

--- model_pipeline.py
+++ model_pipeline.py
@@ -9,15 +9,26 @@ import torch
 import itertools
 import numpy as np
 
-from dataset import get_train_val_test_loaders # TODO, add this function / properly load form dataset loader
-from models.basemodel import BaseModel # TODO, update this to the actual model
-from train_model import _train_epoch, _evaluate_epoch, _predict
-from utils import get_hyper_parameters, make_training_plot, save_training_plot, config
+from diode import DIODE # TODO, add this function / properly load form dataset loader
+from torch.utils.data import DataLoader, SubsetRandomSampler
+from model.model import ImageDepthPredModel
+from train_model import _execute_epoch, _predict
+from utils import get_hyper_parameters, make_training_plot, save_training_plot, config, hold_training_plot
 
 def main():
-    # Data loaders
-    tr_loader, va_loader, te_loader = get_train_val_test_loaders()
-    model = BaseModel()
+    # Setup & Split Dataset
+    dataset = DIODE(meta_fname, data_root, 'val', scene_types):)
+    indices = np.arange(0,len(dataset))
+    np.random.shuffle(indices) # shuffle the indicies
+
+    tr_split_ind = 0.7*len(dataset) # 70% Train
+    va_split_ind = 0.85*len(dataset) # 15% Validation and 15% Test
+    tr_loader = DataLoader(dataset, batch_size=64, shuffle=False, sampler=SubsetRandomSampler(indices[:tr_split_ind]))
+    va_loader = DataLoader(dataset, batch_size=64, shuffle=False, sampler=SubsetRandomSampler(indices[tr_split_ind:va_split_ind]))
+    te_loader = DataLoader(dataset, batch_size=64, shuffle=False, sampler=SubsetRandomSampler(indices[va_split_ind:]))
+
+    # Load model
+    model = ImageDepthPredModel()
 
 
     # Grab hyperparameters for model specified
@@ -30,26 +41,26 @@ def main():
 
     # Grid search on weight decay and learning rate options
     for lr, wd in itertools.product(learning_rate, weight_decay):
-        
         print('Training and evaluating Basemodel with: \tLR = ' +str(lr) + '\tWD = '+str(wd))
+
         # Define loss function, and optimizer
         criterion = torch.nn.CrossEntropyLoss()
         optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
         
-        
+        # Setup plots and metrics results storage
         stats = []
-
         fig, axes = make_training_plot('basemodel')
 
-        # Loop over the entire dataset multiple times
+        # Executee configured number of epochs training + validating
         for epoch in range(0, config('basemodel.num_epochs')):
             # Train model
-            _train_epoch(tr_loader, model, criterion, optimizer)
+            (tr_loader, model, criterion, optimizer)
 
             # Evaluate model
-            _evaluate_epoch(axes, tr_loader, va_loader, model, criterion, epoch, stats)
+            stats = _execute_epoch(axes, tr_loader, va_loader, model, criterion, epoch, stats)
 
         print('Finished Training')
+        
         """
         # TODO, Update these to use the new metrics
         print("Validation Accuracy \t Validation Loss \t Training Accuracy \t Training Loss\n")
@@ -61,7 +72,7 @@ def main():
 
         print('Begin model evaluation...')
 
-        # Evaluate model
+        # Test model
         labels, preds = _predict(te_loader, model)
 
         """
@@ -82,14 +93,14 @@ def main():
             best_lr = lr
             best_wd = wd
         """
-        print('Finished Model Evaluation')
+        print('Finished Model Testing')
 
         save_training_plot(fig, 'basemodel')
 
     print("Best learning rate: {}, best weight_decay: {}".format(best_lr, best_wd))
     print("Weighted F-1: {:.4f}".format(best_wf1))
 
-    utils.hold_training_plot()
+    hold_training_plot()
 
 if __name__ == '__main__':
     main()
